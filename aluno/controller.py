af1ddb9ddc0c3b0421a203687d52135b00709c97
Gabrielly_Anjos <gabrielly.venancio2004@gmail.com>
2024-10-30 17:33:03 -0300

    Alteração pagina aluno

--- aluno/controller.py
+++ aluno/controller.py
@@ -5,23 +5,28 @@ alunos_blueprint = Blueprint('alunos', __name__)
 
 @alunos_blueprint.route('/alunos', methods=['GET'])
 def get_alunos():
-    return jsonify(getlistarAlunos())
+    alunos = listar_alunos()
+    return render_template("alunos.html", alunos=alunos)
 
 @alunos_blueprint.route('/aluno/<int:id_aluno>', methods=['GET'])
 def get_aluno(id_aluno):
     try:
         aluno = getAlunoByid(id_aluno)
-        return jsonify(aluno)
+        nome = request.form['nome']
+        aluno_data = {'nome': nome}
+        atualizar_aluno(id_aluno, aluno_data)
+        return redirect(url_for('alunos.get_aluno', id_aluno=id_aluno))
     except AlunoNaoEncontrado:
         return jsonify({'message': 'Aluno não encontrado'}), 404
     
 
 @alunos_blueprint.route('/aluno', methods=['POST'])
 def create_aluno():
-    try: 
-        data = request.json
-        adicionar_aluno(data)
-        return jsonify(data), 201
+    try:
+        nome = request.form['nome']
+        novo_aluno = {'nome': nome}
+        adicionar_aluno(novo_aluno)
+        return redirect(url_for('alunos.get_alunos')), 201
     except TurmaNaoEncontrada:
         return jsonify({'message': 'Turma não localizada na base!'}),404
 
@@ -40,6 +45,6 @@ def atualizar_aluno(id_aluno):
 def delete_aluno(id_aluno):
     try:
         excluir_aluno(id_aluno)
-        return '', 204
+        return redirect(url_for('alunos.get_alunos')), 204
     except AlunoNaoEncontrado:
         return jsonify({'message': 'Aluno não encontrado'}), 404
